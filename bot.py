4ee18a7fbce1285f82017fa7a2c2ea8bc557b74f
AlexFes <alexeiruxur@gmail.com>
2020-09-05 10:35:17 +0300

    finished bot; todo help message

--- bot.py
+++ bot.py
@@ -2,18 +2,37 @@ import os
 import telebot
 from telebot import types
 import sys
+from pathlib import Path
+import random
+from config import config
+import threading
 
 sys.path.append('stylegan2')
 
 import dnnlib
-import pretrained_networks
+import dnnlib.tflib as tflib
+import numpy as np
 
-token_list = ["1188875292:AAEsXG1kjs9kQDDeKRetB0YolucNBDrVzpg"]
+def generate_images(model_path, dest_path, seed_range):
+    threading.Timer(60.0 * 60.0, generate_images).start()
 
-for token in token_list:
+    sc = dnnlib.SubmitConfig()
+    sc.num_gpus = 1
+    sc.submit_target = dnnlib.SubmitTarget.LOCAL
+    sc.local.do_not_copy_source_files = True
+    sc.run_dir_root = './stylegan2/results'
+    sc.run_desc = 'generate_images'
+
+    dnnlib.submit_run(sc, 'run_generator.generate_images', network_pkl=model_path, seeds=range(seed_range), truncation_psi=1.0, dest=dest_path)
+
+for token, model in config.items():
     bot = telebot.TeleBot(token)
+    model_path = 'models/{}.pkl'.format(model)
+    dest_path = './images/{}'.format(model)
+    seed_range = 1000
+    Path(dest_path).mkdir(parents=True, exist_ok=True)
 
-    _G, _D, Gs = pretrained_networks.load_networks('models/cat.pkl')
+    generate_images(model_path, dest_path, seed_range)
 
     def makeStartKeyboard():
         markup = types.ReplyKeyboardMarkup()
@@ -29,7 +48,10 @@ for token in token_list:
 
     @bot.message_handler(commands=['Генерировать'])
     def generate(message):
-        bot.send_photo(chat_id=message.chat.id, photo=open(temp_output, "rb"))
-        # send_image('cat', bot, message.chat.id)
+        images = os.listdir(dest_path)
+        cur_img = random.choice(images)
+
+        bot.send_photo(chat_id=message.chat.id, photo=open(os.path.join(dest_path, cur_img), "rb"))
+
 
     bot.polling()
